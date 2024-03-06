18512f861c9f9f3b7a11ddc8e2093644271b75fd
Atharva1612 <80762062+Atharva1612@users.noreply.github.com>
2024-03-06 10:39:44 +0530

    main

---
+++ script.js
@@ -0,0 +1,82 @@
+function theme(){
+    
+    document.getElementById("navbar").style.backgroundColor = "black";
+
+}
+function theme1(){
+    document.getElementById("logo1").style.color = "white"
+}
+function theme2(){
+    document.getElementById("logoimg").style.border = "1.5px solid white"
+}
+function theme3(){
+    document.getElementById("logoimg").src ='img/logo.png'
+}
+function theme4(){
+    document.getElementById("navlist").style.color = "white"
+}
+function theme5(){
+    document.getElementById("navlist1").style.color = "white"
+}
+function theme6(){
+    document.getElementById("navlist2").style.color = "white"
+}
+function theme7(){
+    document.getElementById("navlist3").style.color = "white"
+}
+function theme8(){
+    
+    document.getElementById("us").style.backgroundColor = "black";
+
+}
+function theme9(){
+    
+    document.getElementById("svg1").style.cursor ="pointer"
+}
+function theme10(){
+    
+    document.getElementById("svg2").style.cursor ="default"
+
+}
+
+
+//dark theme
+
+
+function good(){
+    
+    document.getElementById("navbar").style.backgroundColor = "white";
+
+}
+function good1(){
+    document.getElementById("logo1").style.color = "black"
+}
+function good2(){
+    document.getElementById("logoimg").style.border = "1.5px solid black"
+}
+function good3(){
+    document.getElementById("logoimg").src ='img/logo1.png'
+}
+function good4(){
+    document.getElementById("navlist").style.color = "black"
+}
+function good5(){
+    document.getElementById("navlist1").style.color = "black"
+}
+function good6(){
+    document.getElementById("navlist2").style.color = "black"
+}
+function good7(){
+    document.getElementById("navlist3").style.color = "black"
+}
+function good8(){
+    
+    document.getElementById("us").style.backgroundColor = "white";
+
+}
+function good9(){
+    document.getElementById("svg1").style.cursor ="default"
+}
+function good10(){
+    document.getElementById("svg2").style.cursor ="pointer"
+}
